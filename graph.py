52985bf5c31c5a768afc07cf1bf7d4f1d3718d41
vidit <vidit.vidit@epfl.ch>
2020-03-28 17:24:36 +0100

    datastructure for volunteers

--- graph.py
+++ graph.py
@@ -1,22 +1,47 @@
 from sklearn.neighbors import KDTree
 import numpy as np
 
-# create a new tree for the given list of volunteers coordinates
-def update_graph(volunteer_coordinates):
-	g = KDTree(volunteer_coordinates,leaf_size=2)
-	return g
+class Volunteers:
+	def __init__(self):
+		self.list = {}
+		self.g = None
+		self.ind2phone = {}
 
-# find closest volunteers, given the search/query coordinate and the graph
-def find_nearest_volunteers(search_coord,graph,num_volunteers=1):
-	dist, ind = graph.query(search_coord,k=num_volunteers)
-	return dist,ind
+	def add_volunteer(self, phone_num,coordinates,is_available,delivery_slots):
+		vol_info = {phone_num:[coordinates,is_available,delivery_slots]}
+		self.list.update(vol_info)
+		self.update_graph()
+
+	# create a new tree with the volunteers coordinates
+	def update_graph(self):
+		coordinates = []
+		for ind,it in enumerate(self.list.items()):
+			key,vals = it
+			coordinates.append(vals[0])
+			self.ind2phone[ind] = key
+
+		coordinates = np.array(coordinates)
+		self.g = KDTree(coordinates,leaf_size=2)
+
+	# find closest volunteers, given the search/query coordinate
+	def find_nearest_volunteers(self,search_coord,num_volunteers=1):
+		dists, inds = self.g.query(search_coord,k=num_volunteers)
+		nearest_volunteers = []
+
+		for i in inds[0]:
+			phone_num = self.ind2phone[i]
+			vol_info = {phone_num:self.list[phone_num]}
+			nearest_volunteers.append(vol_info)
+		return dists, nearest_volunteers
 
 if __name__ == "__main__":
-	X = np.random.randint(10,size=10).reshape(5,2)
-	g = update_graph(X)
-	query = np.random.randint(10,size=2).reshape(1,2)
-	dist,ind = find_nearest_volunteers(query,g,num_volunteers=3)
-	print(X)
-	print(query)
-	print(dist)
-	print(X[ind])
+	vols = Volunteers()
+	vols.add_volunteer(1,[2,9],1,1)
+	vols.add_volunteer(2,[6,1],1,1)
+	vols.add_volunteer(3,[7,6],1,1)
+	vols.add_volunteer(4,[2,3],1,1)
+	vols.add_volunteer(5,[4,0],1,1)
+	query = np.array([[5,1]])
+	dists,nn_vols = vols.find_nearest_volunteers(query,3)
+	print(dists)
+	print(nn_vols)
