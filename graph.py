61d34d1f82553f6151987423a4f83dd238df274b
vidit <vidit.vidit@epfl.ch>
2020-03-30 02:33:43 +0200

    added the logic for db creation with whatsapp; added simple matching with kd tree

--- graph.py
+++ graph.py
@@ -1,6 +1,107 @@
 from sklearn.neighbors import KDTree
 import numpy as np
+import math
 
+# from lat long to x, y, z
+def cartesian(latitude, longitude, elevation = 0):
+    # Convert to radians
+    latitude = latitude * (math.pi / 180)
+    longitude = longitude * (math.pi / 180)
+
+    R = 6371 # 6378137.0 + elevation  # relative to centre of the earth
+    X = R * math.cos(latitude) * math.cos(longitude)
+    Y = R * math.cos(latitude) * math.sin(longitude)
+    Z = R * math.sin(latitude)
+    return [X, Y, Z]
+
+# at the moment matching is supported for one customer. loop breaks if a match is found for one customer.
+def match(vol_list,cust_list,callby='vol_class'):
+	matched_vol = None
+	matched_cust = None
+	matched_time = None
+	#make a kd-tree of volunteers with lat and lonf
+	coordinates = []
+	for ind,it in enumerate(vol_list):
+		cart_coord = cartesian(float(it.latitude),float(it.longitude))
+		coordinates.append(cart_coord)
+
+		coordinates = np.array(coordinates)
+		tree = KDTree(coordinates,leaf_size=2)
+
+	# loop to find nearest vol and then check with overlapping timings.
+	# TODO: for more customers 
+		# remove the previous  matched volunteer
+		# modify the return type handling in all the underlyinng functions
+	for cust in cust_list:
+		query_cart_coord = cartesian(float(cust.latitude),float(cust.longitude))
+		query_cart_coord = np.array(query_cart_coord).reshape(1,3)
+		dist,inds = tree.query(query_cart_coord,k=min(len(vol_list),3))
+
+		inds= inds[0]
+		timing = set(cust.delivery_by)
+		for ind in inds:
+			a = set(vol_list[ind].available_times)
+			overlap = timing.intersection(a)
+			if len(overlap) > 0:
+				matched_time = list(overlap)[0]
+				matched_cust = cust
+				matched_vol = vol_list[ind]
+				break
+
+	if callby == 'vol_class':
+		return matched_vol.number, matched_cust, matched_time
+	else:
+		return matched_cust.number, matched_vol, matched_time
+	# if callby == 'vol_class':
+	# 	return vol_list[0].number, cust_list[0]
+	# else:
+	# 	return cust_list[0].number, vol_list[0]
+
+
+class VolunteerInfo:
+	def __init__(
+            self,
+            number,
+            street_number=None,
+            street=None,
+            city=None,
+            country=None,
+            longitude=None,
+            latitude=None,
+            available_times=None
+    ):
+		self.number = number
+		self.longitude = longitude
+		self.latitude = latitude
+		self.street_number = street_number
+		self.street = street
+		self.city = city
+		self.country = country
+		self.available_times = available_times
+		
+
+class CustomerInfo:
+	def __init__(
+            self,
+            number,
+            street_number=None,
+            street=None,
+            city=None,
+            country=None,
+            longitude=None,
+            latitude=None,
+            order=None,
+            delivery_by=None
+    ):
+		self.number = number
+		self.longitude = longitude
+		self.latitude = latitude
+		self.street_number = street_number
+		self.street = street
+		self.city = city
+		self.country = country
+		self.order= order
+		self.delivery_by = delivery_by
 
 class Volunteers:
 
@@ -9,10 +110,55 @@ class Volunteers:
 		self.list = {}
 		self.g = None
 		self.ind2phone = {}
+		# keep the volunteers waiting to be matched here
+		self.active_vol_list = []
+
+	# update the info of a particular volunteer and find a match if exists
+	def update_data(self, info, active_customers):
+
+		if [*info][0] == 'phone_num' and len([*info]) == 1:
+			cust_info  = {info['phone_num']:VolunteerInfo(number=info['phone_num'])}
+			self.list.update(cust_info)
+			print('Added a volunteer num:{}'.format(info['phone_num']))
+		elif 'address_info' in [*info]:
+			self.list[info['phone_num']].longitude = info['address_info'][0]
+			self.list[info['phone_num']].latitude = info['address_info'][1]
+			self.list[info['phone_num']].street_number = info['address_info'][2]["house_number"]
+			self.list[info['phone_num']].street = info['address_info'][2]["road"]
+			self.list[info['phone_num']].city = info['address_info'][2]["city"]
+			self.list[info['phone_num']].country = info['address_info'][2]["country"]
+			print('updated volunteer num:{} coor:{}'.format(info['phone_num'],info['address_info']))
+		elif 'available_times' in [*info]:
+			self.list[info['phone_num']].available_times = info['available_times']
+			print('updated volunteer num:{} is_available:{}'.format(info['phone_num'],info['available_times']))
+
+		number = info['phone_num']
+		# if all the required fields are updated, move the volunteer to the active list 
+		if self.list[number].longitude is not None and self.list[number].latitude is not None and self.list[number].available_times is not None :
+			self.active_vol_list.append(self.list[number])
+		
+		# if both the customer and voluteer active list is not empty , try to match
+		if len(self.active_vol_list)>0 and len(active_customers)>0:
+			selected_vol_number, selected_cust, matched_time= match(self.active_vol_list,active_customers,callby='vol_class')
+			# no appropriate match found
+			if selected_vol_number is None:
+				return None, None, None
+			selected_vol = self.list[selected_vol_number]
+			#remove the selected vol from the active list
+			self.active_vol_list.remove(self.list[selected_vol_number])
+			#remove the selected customer from the active list
+			active_customers.remove(selected_cust)
+			return selected_vol, selected_cust, matched_time
+		else:
+			return None, None, None
+
+	def get_address(self,number):
+		address = self.list[number].street + ' ' + self.list[number].street_number + ' ' + self.list[number].city
+		return address
 
 	# add a volunteer with their details
-	def add_volunteer(self, phone_num,coordinates,is_available,delivery_slots):
-		vol_info = {phone_num:[coordinates,is_available,delivery_slots]}
+	def add_volunteer(self, phone_num, volunteer_info):
+		vol_info = {phone_num:volunteer_info}
 		self.list.update(vol_info)
 		self.update_graph()
 
@@ -44,16 +190,68 @@ class Customers:
 	# init for the class
 	def __init__(self):
 		self.list = {}
+		# keep the customers waiting to be matched here
+		self.active_cust_list = []
+
+	# update the info of a particular customer and find a match if exists
+	def update_data(self, info, active_vols):
+		
+		if [*info][0] == 'phone_num' and len([*info]) == 1:
+			cust_info = {info['phone_num']:CustomerInfo(number=info['phone_num'])}
+			self.list.update(cust_info)
+			print('Added a customer num:{}'.format(info['phone_num']))
+		elif 'address_info' in [*info]:
+			self.list[info['phone_num']].longitude = info['address_info'][0]
+			self.list[info['phone_num']].latitude = info['address_info'][1]
+			self.list[info['phone_num']].street_number = info['address_info'][2]["house_number"]
+			self.list[info['phone_num']].street = info['address_info'][2]["road"]
+			self.list[info['phone_num']].city = info['address_info'][2]["city"]
+			self.list[info['phone_num']].country = info['address_info'][2]["country"]
+			print('updated customer num:{} coor:{}'.format(info['phone_num'],info['address_info']))
+		elif 'order' in [*info]:
+			self.list[info['phone_num']].order = info['order']
+			print('updated customer num:{} order'.format(info['phone_num']))
+		elif 'delivery_by' in [*info]:
+			self.list[info['phone_num']].delivery_by = info['delivery_by']
+			print('updated customer num:{} delivery_by:{}'.format(info['phone_num'],info['delivery_by']))
+
+		number = info['phone_num']
+		# if all the required fields are updated, move the customer to the active list 
+		if self.list[number].longitude is not None and self.list[number].latitude is not None \
+				and self.list[number].order is not None and self.list[number].delivery_by is not None:
+			self.active_cust_list.append(self.list[number])
+		
+		# if both the customer and voluteer active list is not empty , try to match
+		if len(self.active_cust_list)>0 and len(active_vols)>0:
+			selected_cust_number, selected_vol, matched_time = match(active_vols,self.active_cust_list,callby='cust_class')
+			# no appropriate match found
+			if selected_cust_number is None:
+				return None, None, None
+			selected_cust = self.list[selected_cust_number]
+			#remove the selected customer from the active list
+			self.active_cust_list.remove(self.list[selected_cust_number])
+			#remove the selected vol from the active list
+			active_vols.remove(selected_vol)
+			return selected_vol, selected_cust, matched_time
+		else:
+			return None, None, None
+
+	def get_address(self,number):
+		address = self.list[number].street + ' ' + self.list[number].street_number + ' ' + self.list[number].city	
+		return address
 
 	# add a new customer with their details
-	def add_customer(self, phone_num, coordinates, order, deliverbytime):
-		cust_info = {phone_num:[coordinates,order,deliverbytime]}
+	def add_customer(self, phone_num, customer_info):
+		cust_info = {phone_num:customer_info}
 		self.list.update(cust_info)
 
 	# remove a customer
 	def remove_customer(self, phone_num):
 		self.list.pop(phone_num,None)
 
+	# flag to set if customer is served
+	def set_serve():
+		self.is_served = True
 
 if __name__ == "__main__":
 	vols = Volunteers()
--- run_server.py
+++ run_server.py
@@ -5,12 +5,28 @@ from twilio.twiml.messaging_response import MessagingResponse
 import requests
 import os
 import json
+from graph import Volunteers, Customers
+import SimpleEvent.events
+from twilio.rest import Client
 
 
 app = Flask(__name__)
 
 locationiq_url = "https://us1.locationiq.com/v1/reverse.php"
 
+# to maintain the data of all the customers and volunteers. 
+# They are dictionary with phone number as key.
+vols = Volunteers()
+cuts = Customers()
+
+# hack to test when have one phone number to test
+vol_timings = {'1':'Morning','2':'Afternoon','3':'Evening'}
+cust_timings = {'a':'Morning','b':'Afternoon','c':'Evening'}
+
+# to send custom message
+account_sid = os.environ.get('TWILIO_SID')
+auth_token = os.environ.get('TWILIO_TOKEN')
+client = Client(account_sid, auth_token)
 
 @app.route("/whatsapp", methods=['GET', 'POST'])
 def whatsapp_parser():
@@ -23,22 +39,57 @@ def whatsapp_parser():
     number = request.values.get("From", None)
     n_media = int(request.values.get("NumMedia"))
 
+    # assuming the volunteer's message starts with "volunteer" and
+    # customer's with "customer"
+    
+    # dummy reply
+    reply_for_text = "Ahoy! Thanks so much for your message."
+
+    # when message is text
+    if text is not None:
+
+        # update the list of customers
+        if text.lower() == "customer":
+            cuts.update_data({'phone_num':number},vols.active_vol_list)
+            reply_for_text = "Your number is registered as a Customer. Tell us your possible delivery timings. \n Send A for Morning 8am-12pm \n B for Afternoon 12pm-4pm \n C for Evening 4pm-8pm\n You can choose multiple timings."
+        
+        # update the list of volunteers
+        elif text.lower() == "volunteer":
+            vols.update_data({'phone_num':number},cuts.active_cust_list)
+            reply_for_text = "Your number is registered as a Volunteer. Tell us your availability. \n Send 1 for Morning 8am-12pm \n 2 for Afternoon 12pm-4pm \n 3 for Evening 4pm-8pm\n You can choose multiple timings."
+        
+        # update the particular volunteer's timing
+        elif text.split(' ')[0] in ['1','2','3']:
+            # assume text to be space separated
+            available_times = text.split(' ')
+            available_times = [vol_timings[t] for t in available_times]
+            vols.update_data({'phone_num':number,'available_times':available_times},cuts.active_cust_list)
+            opted_times = ' '.join(available_times)
+            reply_for_text = f"Your availability is set for {opted_times}. Please send your location."
+        
+        # update the particular customer's timing
+        elif text.split(' ')[0].lower() in ["a","b","c"]:
+            # assume text to be space separated
+            available_times = text.split(' ')
+            available_times = [cust_timings[t.lower()] for t in available_times]
+            cuts.update_data({'phone_num':number,'delivery_by':available_times},vols.active_vol_list)
+            opted_times = ' '.join(available_times)
+            reply_for_text = f"Your delivery timings are set for {opted_times}. Please take a picture of your grocery list"
+
     # Start our response
     resp = MessagingResponse()
 
+    # save the grocery list image url 
     if n_media > 0:
         media_url = request.values.get("MediaUrl0")
-        r = requests.get(media_url, allow_redirects=True, stream=True)
-        with open("local-filename.jpg", 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
+        cuts.update_data({'phone_num':number,'order':media_url},vols.active_vol_list)
 
-        resp.message(body="Got your picture!")
+        resp.message(body="Got your picture!. Send us your location.")
 
+    # handling for location and since this the last step in the sequence of instruction, 
+    # we try to find a match here        
     elif longitude is not None:
         # return corresponding address for now
-
         data = {
             'key': os.environ.get('LOCATIONIQ_TOKEN'),
             'lat': str(latitude),
@@ -50,16 +101,74 @@ def whatsapp_parser():
         d = json.loads(response.text)
         address = d["display_name"]
         address_dict = d["address"]
-        house_number = address_dict["house_number"]
-        road = address_dict["road"]
-        city = address_dict["city"]
-        country = address_dict["country"]
 
-        resp.message(body=f"Your address : {address}")
+        reply_text_body=f"Your address : {address}."
+        
+        # check if the location sent was of a customer or volunteer, by comparing the keys.
+        if number in cuts.list.keys():
+            #update the customer location and receive the match
+            vol,cust,time = cuts.update_data({'phone_num':number,'address_info':[longitude,latitude,address_dict]},vols.active_vol_list)
+            
+            # check if a match was found
+            if vol is  None:
+                # reply to the customer
+                reply_text_body += "We'll get back to you with info of your friendly neighbor."
+            else:
+                # reply to the customer
+                reply_text_body += "Your Neighbor:{} has agreed to do the delivery in the {}. You can get in touch with them.".format(vol.number[9:], time)
+                resp.message(body="")
+                message = client.messages.create(
+                        from_='whatsapp:+14155238886',
+                        body= reply_text_body,
+                        to='whatsapp:{}'.format(cust.number[9:])
+                        )
+                print(message.sid)
 
+                # reply to the volunteer
+                vol_message = "We found a customer:{} needing your help. Their address is :{}. You can see their grocery list in the image attached to be delivery by the {}".format(cust.number[9:], cuts.get_address(cust.number), time)
+               
+                message = client.messages.create(
+                        from_='whatsapp:+14155238886',
+                        body= vol_message,
+                        media_url=[cust.order],
+                        to='whatsapp:{}'.format(vol.number[9:])
+                        )
+                print(message.sid)
+            resp.message(body=reply_text_body)
+
+        # check if the location sent was of a customer or volunteer, by comparing the keys.
+        elif number in vols.list.keys():
+            #update customer location and receive the match
+            vol,cust,time = vols.update_data({'phone_num':number,'address_info':[longitude,latitude,address_dict]},cuts.active_cust_list)
+            
+            # check if a match was found
+            if vol is  None:
+                # reply to the volunteer
+                reply_text_body += "Thanks for your help. We'll get back to with the info of the delivery."
+                resp.message(body=reply_text_body)
+            else:
+                # reply to the volunteer
+                reply_text_body += "\nThanks for your help. We found {} at this address:{}. Attached is the image of their grocery list to be delivery by the {}.".format(cust.number[9:], cuts.get_address(cust.number), time)
+                resp.message(body="")
+                message = client.messages.create(
+                        from_='whatsapp:+14155238886',
+                        body= reply_text_body,
+                        media_url= [cust.order],
+                        to='whatsapp:{}'.format(vol.number[9:])
+                        )
+                print(message.sid)
+
+                # reply to the customer
+                cust_message = "We found a neighbor:{} ready to help you and make delivery by the {}. You can get in touch with them.".format(vol.number[9:], time)
+                message = client.messages.create(
+                        from_='whatsapp:+14155238886',
+                        body= cust_message,
+                        to='whatsapp:{}'.format(cust.number[9:])
+                        )
+                print(message.sid)
+        
     else:
-
-        resp.message(body="Ahoy! Thanks so much for your message.")
+        resp.message(body=reply_for_text)
 
     return str(resp)
 
